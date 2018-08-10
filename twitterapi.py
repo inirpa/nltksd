feff175fc08ebcfed611bac49505b39ad02cb927
suman khadka <sumankhadk@gmail.com>
2018-08-10 15:58:05 +0545

    first commit

---
+++ twitterapi.py
@@ -0,0 +1,40 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sentiment_mod as s
+	
+# from twitterapistuff import *
+
+#consumer key, consumer secret, access token, access secret.
+ckey="GPf9z41bb5f1cUGXZGJcTTvct"
+csecret="BoILguGn8UcPWKnt5qR0aFsVa7Sg5M5F0ZWaKIndpwu3mfw2Ik"
+atoken="725330079925043200-NzhEgRTjHX6A2FfLIsRtCXFe5kbqr5k"
+asecret="jRvYVdVe0I7lpq40B0yAdcWwgwHtQyx5iKRY6fzPL6r1Q"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+
+		all_data = json.loads(data)
+
+		tweet = all_data["text"]
+		sentiment_value, confidence = s.sentiment(tweet)
+		print(tweet, sentiment_value, confidence)
+
+		if confidence*100 >= 80:
+			output = open("twitter-out.txt","a")
+			output.write(sentiment_value)
+			output.write('\n')
+			output.close()
+
+		return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["happy"], languages=['nl'])
